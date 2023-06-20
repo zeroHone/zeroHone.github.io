8efefaa79466337367ff6b899a4a1a2a12220f00
zeroHone <hosein.habibifar@gmail.com>
2023-06-20 15:26:43 +0330

    Create serviceworker.js

---
+++ serviceworker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ['index.html','offline.html'];
+
+// Install SW
+self.addEventListener('install' , (event) =>{
+event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then((cache)=>{
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+    })
+)
+
+
+});
+// Listen for requests
+self.addEventListener('fetch' , (event) => {
+
+    event.respondWith(
+        caches.match(event.request)
+        .then(()=>{
+            return fetch(event.request)
+            .catch(()=> caches.match('offline.html'))
+        })
+    )
+
+});
+//Activate the SW
+
+
